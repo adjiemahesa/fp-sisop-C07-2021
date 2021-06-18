665558d1c388138a929ec9e480a29c160b78ed17
adjiemahesa <adjiemahesa75@gmail.com>
2021-06-18 20:27:07 +0700

    Soal FP

--- database/program_database.c
+++ database/program_database.c
@@ -1,4 +1,17 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <unistd.h>
+#include <ctype.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <time.h>
+#include <dirent.h>
+#include <errno.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <pthread.h>
 
 int main(){
     return 0;
